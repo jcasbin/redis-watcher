56f33d16328e33693e71c7936f21486e11f5dc7b
viking <1162777073@qq.com>
2022-06-02 00:26:44 +0800

    feat: add jedis pool config

--- src/test/java/org/casbin/test/RedisWatcherTest.java
+++ src/test/java/org/casbin/test/RedisWatcherTest.java
@@ -5,12 +5,15 @@ import org.casbin.watcher.RedisWatcher;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import redis.clients.jedis.JedisPoolConfig;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class RedisWatcherTest {
-    private RedisWatcher redisWatcher;
+    private RedisWatcher redisWatcher,redisConfigWatcher;
     private final String expect="update msg";
+    private final String expectConfig="update msg for config";
 
     /**
      * You should replace the initWatcher() method's content with your own Redis instance.
@@ -18,9 +21,26 @@ public class RedisWatcherTest {
     @Before
     public void initWatcher(){
         String redisTopic = "jcasbin-topic";
+        String redisConfig = "jcasbin-config";
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(2);
+        config.setMaxWaitMillis(100 * 1000);
         redisWatcher = new RedisWatcher("127.0.0.1",6379, redisTopic, 2000, "foobared");
+        redisConfigWatcher = new RedisWatcher(config,"127.0.0.1",6376, redisConfig, 2000, "foobaredConfig");
         Enforcer enforcer = new Enforcer();
         enforcer.setWatcher(redisWatcher);
+        Enforcer configEnforcer = new Enforcer();
+        configEnforcer.setWatcher(redisConfigWatcher);
+    }
+
+    @Test
+    public void testConfigUpdate() throws InterruptedException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        redisConfigWatcher.setUpdateCallback(()-> System.out.print(expectConfig) );
+        redisConfigWatcher.update();
+        Thread.sleep(100);
+        Assert.assertEquals(expectConfig, expectConfig);
     }
 
     @Test
@@ -40,6 +60,12 @@ public class RedisWatcherTest {
         });
         redisWatcher.update();
         Thread.sleep(100);
+
+        redisConfigWatcher.setUpdateCallback((s) -> {
+            System.out.print(s);
+        });
+        redisConfigWatcher.update();
+        Thread.sleep(100);
     }
 
     @Test
@@ -47,5 +73,9 @@ public class RedisWatcherTest {
         String redisTopic = "jcasbin-topic";
         RedisWatcher redisWatcherWithoutPassword = new RedisWatcher("127.0.0.1", 6378, redisTopic);
         Assert.assertNotNull(redisWatcherWithoutPassword);
+
+        String redisConfig = "jcasbin-config";
+        RedisWatcher redisConfigWatcherWithoutPassword = new RedisWatcher("127.0.0.1", 6377, redisConfig);
+        Assert.assertNotNull(redisConfigWatcherWithoutPassword);
     }
 }
